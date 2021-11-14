2a86735edb454131315123f75cae1fa988ce339f
briandelaney <delaneybrian6@gmail.com>
2021-11-14 16:59:33 +0000

    request response

--- 6-Request-Reply/client.py
+++ 6-Request-Reply/client.py
@@ -1,15 +1,19 @@
 import pika
 import uuid
 
+<<<<<<< HEAD
 def on_reply_message_received(ch, method, properties, body):
     print(f"reply recieved: {body}")
 
+=======
+>>>>>>> b3416ae (request response)
 connection_parameters = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
+<<<<<<< HEAD
 reply_queue = channel.queue_declare(queue='', exclusive=True)
 
 channel.basic_consume(queue=reply_queue.method.queue, auto_ack=True,
@@ -28,3 +32,33 @@ channel.basic_publish('', routing_key='request-queue', properties=pika.BasicProp
 print("Starting Client")
 
 channel.start_consuming()
+=======
+channel.queue_declare(queue='request_queue')
+
+reply_queue = channel.queue_declare(queue='', exclusive=True)
+
+correlation_id = str(uuid.uuid4())
+
+message = "This Message needs a response"
+
+channel.basic_publish(exchange='', routing_key='request_queue',
+            properties=pika.BasicProperties(
+                reply_to=reply_queue.method.queue,
+                correlation_id=correlation_id,
+            ),
+            body=message)
+
+print(f"sent : {correlation_id} : {message}")
+
+def on_message_received(ch, method, properties, body):
+    print(f"received: {properties.correlation_id} : {body}")
+
+channel.basic_consume(queue=reply_queue.method.queue, auto_ack=True,
+    on_message_callback=on_message_received)
+
+print("Client Starting Consuming")
+
+channel.start_consuming()
+
+connection.close()
+>>>>>>> b3416ae (request response)
--- 6-Request-Reply/server.py
+++ 6-Request-Reply/server.py
@@ -1,15 +1,19 @@
 import pika
 
+<<<<<<< HEAD
 def on_request_message_received(ch, method, properties, body):
     print(f"Received Request: {properties.correlation_id}")
     ch.basic_publish('', routing_key=properties.reply_to, body=f'Hey its your reply to {properties.correlation_id}')
 
+=======
+>>>>>>> b3416ae (request response)
 connection_parameters = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
+<<<<<<< HEAD
 channel.queue_declare(queue='request-queue')
 
 channel.basic_consume(queue='request-queue', auto_ack=True,
@@ -18,3 +22,28 @@ channel.basic_consume(queue='request-queue', auto_ack=True,
 print("Starting Server")
 
 channel.start_consuming()
+=======
+channel.queue_declare(queue='request_queue')
+
+def on_message_received(ch, method, properties, body):
+
+    print(f"recieved : {properties.correlation_id} : {body}")
+
+    response_message = "This is the response message"
+
+    channel.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     properties=pika.BasicProperties(correlation_id=properties.correlation_id),
+                     body=response_message)
+
+    print(f"sent : {properties.correlation_id} : {response_message}")
+
+channel.basic_consume(queue='request_queue', auto_ack=True,
+    on_message_callback=on_message_received)
+
+print("Server Starting Consuming")
+
+channel.start_consuming()
+
+connection.close()
+>>>>>>> b3416ae (request response)
